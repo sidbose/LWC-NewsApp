8b763e1bc4b678f8d4230701a358e7a2cc4ee1e2
Sid <siddharthabose@live.com>
2020-09-11 20:44:40 +0530

    To do round the logic fixed

--- news-app/main/default/lwc/toDo/toDo.js
+++ news-app/main/default/lwc/toDo/toDo.js
@@ -1,23 +1,49 @@
-import { LightningElement, wire } from 'lwc';
-import addToDoList from '@salesforce/apex/ToDoController.addToDoList'
+import { LightningElement, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { reduceErrors } from 'c/ldsUtils';
+
+import addToDoItem from '@salesforce/apex/ToDoController.addToDoItem'
 import getToDoItems from '@salesforce/apex/ToDoController.getToDoItems'
 
 export default class ToDo extends LightningElement {
-    todos = [];
+    // @track is required for array and object properties 
+    @track todos = [];
 
-    //wiring to a function
-    @wire(getToDoItems)
-    getCurrentToDoItems({ data, error }) {
-        if (data) {
-            this.todos = data;
-        }
-        else if (error) {
-            this.showToastNotification(error);
-        }
+    connectedCallback() {
+        getToDoItems()
+            .then(result => {
+                this.todos = result;
+            })
+            .catch(error => {
+                this.showToastNotification(error);
+            })
     }
 
-    addtohandler(event) {
-        todos.push(this.template.query("lightning-input").value);
+    addtohandler() {
+        const inputbox = this.template.querySelector("lightning-input");
+        addToDoItem({
+            todoItem: inputbox.value,
+            completed: false
+        })
+            .then((res) => {
+                console.log('addToDoItem() promise res: ' + res);
+                // Update to do items by fetching fresh results. Returns a promise as it is not wired
+                getToDoItems()
+                    .then((result) => {
+                        if (result) {
+                            console.log('getToDoItems() promise result: ' + result);
+                            inputbox.value = '';
+                            this.todos = result;
+                        }
+                    })
+                    .catch((error) => {
+                        this.showToastNotification(error);
+                    })
+
+            })
+            .catch((err) => {
+                this.showToastNotification(err);
+            });
     }
 
     //helper method
